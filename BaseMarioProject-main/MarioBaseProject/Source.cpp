2b5a4b62429394e860e3176f0cf62a4631f2daae
ArisRay002 <115143853+ArisRay002@users.noreply.github.com>
2023-03-09 10:12:14 +0000

    Tutorial2

--- BaseMarioProject-main/MarioBaseProject/Source.cpp
+++ BaseMarioProject-main/MarioBaseProject/Source.cpp
@@ -5,14 +5,25 @@
 #include <iostream>
 
 using namespace std;
+
+SDL_Window* g_window = nullptr;
+SDL_Renderer* g_renderer = nullptr;
+SDL_Texture* g_texture = nullptr;
+bool InitSDL();
+void CloseSDL();
+bool Update();
+void Render();
+SDL_Texture* LoadTextureFromFile(string path);
+void FreeTexture();
 int main(int argc, char* args[])
 {
-	SDL_Window* g_window = nullptr;
-	if (InitSDL())
+	
+	bool quit = false;
+	while (!quit)
 	{
-		SDL_Delay(5000);
+		Render();
+		quit = Update();
 	}
-	CloseSDL();
 
 	return 0;
 }
@@ -34,6 +45,28 @@ bool InitSDL()
 			return false;
 		}
 	}
+	
+	g_renderer = SDL_CreateRenderer(g_window, -1, SDL_RENDERER_ACCELERATED);
+	g_texture = LoadTextureFromFile("Images/test.bmp");
+	if (g_texture == nullptr)
+	{
+		return false;
+	}
+	if (g_renderer != nullptr)
+	{
+		int imageFlags = IMG_INIT_PNG;
+		if (!(IMG_Init(imageFlags) & imageFlags))
+		{
+			cout << "SDL_Image could not initialise. Error: " << IMG_GetError();
+			return false;
+		}
+	}
+	else
+	{
+		cout << "Renderer could not initialise. Error: " << SDL_GetError();
+		return false;
+	}
+	
 }
 
 void CloseSDL()
@@ -43,4 +76,64 @@ void CloseSDL()
 
 	IMG_Quit();
 	SDL_Quit();
+
+	FreeTexture();
+	SDL_DestroyRenderer(g_renderer);
+	g_renderer = nullptr;
+}
+
+bool Update()
+{
+	SDL_Event e;
+
+	SDL_PollEvent(&e);
+
+	switch (e.type)
+	{
+	case SDL_QUIT:
+		return true;
+		break;
+	}
+	return false;
+}
+
+void Render()
+{
+	SDL_SetRenderDrawColor(g_renderer, 0xFF, 0xFF, 0xFF, 0xFF);
+	SDL_RenderClear(g_renderer);
+	SDL_Rect renderLocation = { 0,0,SCREEN_WIDTH, SCREEN_HEIGHT };
+	SDL_RenderCopyEx(g_renderer, g_texture, NULL, &renderLocation, 0, NULL, SDL_FLIP_NONE);
+	SDL_RenderPresent(g_renderer);
+}
+
+SDL_Texture* LoadTextureFromFile(string path)
+{
+	FreeTexture();
+	SDL_Texture* p_texture = nullptr;
+
+	SDL_Surface* p_surface = IMG_Load(path.c_str());
+	if (p_surface != nullptr)
+	{
+		p_texture = SDL_CreateTextureFromSurface(g_renderer, p_surface);
+		if (p_texture == nullptr)
+		{
+			cout << "Unable to create texture from surface. Error: " << SDL_GetError();
+		}
+		SDL_FreeSurface(p_surface);
+	}
+	else 
+	{
+		cout << "Unable to create texture from surface. Error: " << SDL_GetError();
+	}
+	return p_texture;
+	return nullptr;
+}
+
+void FreeTexture()
+{
+	if (g_texture != nullptr)
+	{
+		SDL_DestroyTexture(g_texture);
+		g_texture = nullptr;
+	}
 }
