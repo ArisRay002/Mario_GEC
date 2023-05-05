a4ba6eef6535e0412b2625c3d0656c140b94ef38
Ray Lo <raylo1130@gmail.com>
2023-05-05 21:58:25 +0100

    Update CharacterMario.cpp

--- BaseMarioProject-main/MarioBaseProject/CharacterMario.cpp
+++ BaseMarioProject-main/MarioBaseProject/CharacterMario.cpp
@@ -1,3 +1,3 @@
 #include "CharacterMario.h"
 
-CharacterMario::CharacterMario(SDL_Renderer* renderer, string imagePath, Vector2D start_postition):Character(renderer, imagePath, start_postition);
+CharacterMario::CharacterMario(SDL_Renderer* renderer, string imagePath, Vector2D start_position) : Character(renderer, imagePath, start_position)
